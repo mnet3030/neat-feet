92785721e0ced537e76e1bafef612635fc815a08
Alia Mahmoud <aliahmahmoud52@gmail.com>
2019-03-30 12:50:24 +0200

    Adding saveImage Servlet

--- src/main/java/com/imagine/neatfeat/controller/customer/servlets/RegisterServlet.java
+++ src/main/java/com/imagine/neatfeat/controller/customer/servlets/RegisterServlet.java
@@ -10,12 +10,14 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
+@MultipartConfig
 public class RegisterServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
