d17f293af2d92af1b71f03afc476b1311edc13bd
AmrElkady <amrelkady93@gmail.com>
2019-04-02 18:36:45 +0200

    edit Ajax functions and slove checkoutAjax problem

--- src/main/webapp/view/customer/html/js/checkoutAjax.js
+++ src/main/webapp/view/customer/html/js/checkoutAjax.js
@@ -1,20 +1,32 @@
 
-$("#totalprice").val("545"+" EGP");
 function pluse(btn) {
     var id=btn.previousElementSibling.previousElementSibling.previousElementSibling.value;
-    $.post("checkout?action=increse&productid="+id+"",
+    $.post("checkout?action=increase&productid="+id+"",
         function (data,state) {
             $("#totalprice").html(data+" EGP");
+            $("#afterAddServices").html(data+" EGP");
 
         });
 
-    }
+}
 function minus(btn) {
     var id=btn.previousElementSibling.value;
 
     $.post("checkout?action=decrease&productid="+id+"",
         function (data,state) {
             $("#totalprice").html(data+" EGP");
+            $("#afterAddServices").html(data+" EGP");
+        });
+
+}
+
+function remove(id) {
+
+    $.post("checkout?action=delete&productid="+id+"",
+        function (data,state) {
+            $("#totalprice").html(data+" EGP");
+            $("#afterAddServices").html(data+" EGP");
+
         });
 
 }
