4d0a05f32369707fa3fbf08b599741e6b0537083
Alia Mahmoud <aliahmahmoud52@gmail.com>
2019-04-07 19:22:01 +0200

    addin guser servlet

---
+++ src/main/java/com/imagine/neatfeat/controller/admin/servlets/UserServlet.java
@@ -0,0 +1,47 @@
+package com.imagine.neatfeat.controller.admin.servlets;
+
+import com.imagine.neatfeat.model.dal.dao.UserDAO;
+import com.imagine.neatfeat.model.dal.entity.Product;
+import com.imagine.neatfeat.model.dal.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+public class UserServlet  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*Mahmoud Shereif*/
+
+        /*Amr El Kady*/
+
+        /*Alia Mahmoud*/
+
+       
+        /*Amer Salah*/
+
+        /*Nouran Habib*/
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*Mahmoud Shereif*/
+
+        /*Amr El Kady*/
+
+        /*Alia Mahmoud*/
+
+        /*Amer Salah*/
+
+        /*Nouran Habib*/
+
+    }
+}
