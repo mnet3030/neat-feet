553285718f8a7326a8660040c1679dc51e0bc894
Alia Mahmoud <aliahmahmoud52@gmail.com>
2019-04-19 17:54:03 +0200

    add multiple images to product

--- src/main/java/com/imagine/neatfeat/controller/admin/servlets/ImageSaving.java
+++ src/main/java/com/imagine/neatfeat/controller/admin/servlets/ImageSaving.java
@@ -22,7 +22,10 @@ public class ImageSaving extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //--------get the image part ----------------------------------
         final Part imagePart = req.getPart("image");
-
+        final Part imagePart1 = req.getPart("image1");
+        final Part imagePart2 = req.getPart("image2");
+        final Part imagePart3 = req.getPart("image3");
+        //System.out.println("image name" +imagePart.getName());
         if (imagePart.getSize() > 0) {
             //-----------get user to get user ID --------------------------
             //--------to be used as image name-----------------------------
@@ -52,6 +55,133 @@ public class ImageSaving extends HttpServlet {
                 }
                 product.setMainPhotoUrl(imageName);
                 req.setAttribute("product", product);
+                //---------------------------------------------------------------------------------------------------
+            } catch (FileNotFoundException fne) {
+                fne.printStackTrace();
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+                if (imageContent != null) {
+                    imageContent.close();
+                }
+            }
+        }
+        //-----------------------------------------------------------------------------------------------------------
+        //save second image
+        if (imagePart1.getSize() > 0) {
+            //-----------get user to get user ID --------------------------
+            //--------to be used as image name-----------------------------
+            Product product = (Product) req.getAttribute("product");
+            UUID productUUID = product.getId();
+            String productID = productUUID.toString();
+            String imageName = "product" + productID + "1.jpg";
+            //---------define path where images will be saved--------------
+
+            String appPath = req.getServletContext().getRealPath("");
+            String savePath = appPath + File.separator + "ProductIamges";
+            //--------------------------------------------------------------
+            OutputStream out = null;
+            InputStream imageContent = null;
+            try {
+                File imageSaveDirectory = new File(savePath);
+                if (!imageSaveDirectory.exists()) {
+                    imageSaveDirectory.mkdir();
+                }
+                out = new FileOutputStream(new File(savePath + File.separator
+                        + imageName));
+                imageContent = imagePart1.getInputStream();
+                int read = 0;
+                final byte[] bytes = new byte[2048];
+                while ((read = imageContent.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+
+                //---------------------------------------------------------------------------------------------------
+            } catch (FileNotFoundException fne) {
+                fne.printStackTrace();
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+                if (imageContent != null) {
+                    imageContent.close();
+                }
+            }
+        }
+        //-----------------------------------------------------------------------------------------------------------
+        //save third image
+        if (imagePart2.getSize() > 0) {
+            //-----------get user to get user ID --------------------------
+            //--------to be used as image name-----------------------------
+            Product product = (Product) req.getAttribute("product");
+            UUID productUUID = product.getId();
+            String productID = productUUID.toString();
+            String imageName = "product" + productID + "2.jpg";
+            //---------define path where images will be saved--------------
+
+            String appPath = req.getServletContext().getRealPath("");
+            String savePath = appPath + File.separator + "ProductIamges";
+            //--------------------------------------------------------------
+            OutputStream out = null;
+            InputStream imageContent = null;
+            try {
+                File imageSaveDirectory = new File(savePath);
+                if (!imageSaveDirectory.exists()) {
+                    imageSaveDirectory.mkdir();
+                }
+                out = new FileOutputStream(new File(savePath + File.separator
+                        + imageName));
+                imageContent = imagePart2.getInputStream();
+                int read = 0;
+                final byte[] bytes = new byte[2048];
+                while ((read = imageContent.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+
+                //---------------------------------------------------------------------------------------------------
+            } catch (FileNotFoundException fne) {
+                fne.printStackTrace();
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+                if (imageContent != null) {
+                    imageContent.close();
+                }
+            }
+        }
+        //-----------------------------------------------------------------------------------------------------------
+        //save fourth image
+        if (imagePart3.getSize() > 0) {
+            //-----------get user to get user ID --------------------------
+            //--------to be used as image name-----------------------------
+            Product product = (Product) req.getAttribute("product");
+            UUID productUUID = product.getId();
+            String productID = productUUID.toString();
+            String imageName = "product" + productID + "3.jpg";
+            //---------define path where images will be saved--------------
+
+            String appPath = req.getServletContext().getRealPath("");
+            String savePath = appPath + File.separator + "ProductIamges";
+            //--------------------------------------------------------------
+            OutputStream out = null;
+            InputStream imageContent = null;
+            try {
+                File imageSaveDirectory = new File(savePath);
+                if (!imageSaveDirectory.exists()) {
+                    imageSaveDirectory.mkdir();
+                }
+                out = new FileOutputStream(new File(savePath + File.separator
+                        + imageName));
+                imageContent = imagePart3.getInputStream();
+                int read = 0;
+                final byte[] bytes = new byte[2048];
+                while ((read = imageContent.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+
+                //---------------------------------------------------------------------------------------------------
             } catch (FileNotFoundException fne) {
                 fne.printStackTrace();
             } finally {
