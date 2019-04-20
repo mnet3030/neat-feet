de855d79dcbe417ffdbbd30587062dfe1b2fea5f
Mahmoud Shereif <Eng.Mahmoud.Shereif@gmail.com>
2019-04-20 16:49:49 +0200

    Fix exceptions in ProductServlet

--- src/main/java/com/imagine/neatfeat/controller/customer/servlets/ProductServlet.java
+++ src/main/java/com/imagine/neatfeat/controller/customer/servlets/ProductServlet.java
@@ -3,10 +3,14 @@ package com.imagine.neatfeat.controller.customer.servlets;
 import com.imagine.neatfeat.model.dal.dao.ProductDAO;
 import com.imagine.neatfeat.model.dal.entity.Category;
 import com.imagine.neatfeat.model.dal.entity.Product;
+import com.imagine.neatfeat.model.dal.entity.User;
+import com.imagine.neatfeat.model.dal.servletsdaos.LoginDao;
 import com.imagine.neatfeat.model.dal.servletsdaos.ResultDao;
 import com.imagine.neatfeat.model.dal.utility.CheckoutUtility;
 import com.imagine.neatfeat.model.dal.utilityPojos.Item;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -22,16 +26,17 @@ import java.util.UUID;
 
 public class ProductServlet extends HttpServlet {
     ResultDao resultDao;
-    Session session;
+    SessionFactory sessionFactory;
     @Override
     public void init() throws ServletException {
         resultDao = new ResultDao();
-        session = (Session)getServletContext().getAttribute("session");
+        sessionFactory = (SessionFactory) getServletContext().getAttribute("sessionFactory");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /*Mahmoud Shereif*/
+
         HttpSession httpSession=request.getSession(false);
         if(httpSession!=null && httpSession.getAttribute("user")!=null){
             request.setAttribute("loggedIn", true);
@@ -46,35 +51,51 @@ public class ProductServlet extends HttpServlet {
 
         if(request.getParameter("id") != null) {
 
-            String productId = request.getParameter("id");
-            Product product = new Product();
-            ProductDAO productDao = new ProductDAO(session);
+            Session session = sessionFactory.getCurrentSession();
 
-            UUID uuid = UUID.fromString(productId);
-            product = productDao.getByPrimaryKey(uuid);
+            Transaction tx = null;
+
+            try {
+                tx = session.beginTransaction();
+
+                String productId = request.getParameter("id");
+                Product product = new Product();
+                ProductDAO productDao = new ProductDAO(session);
 
 
-            request.setAttribute("product", product);
+                UUID uuid = UUID.fromString(productId);
+                product = productDao.getByPrimaryKey(uuid);
 
-            List<Item> cart= (List<Item>) request.getSession(false).getAttribute("cartProduct");
-            CheckoutUtility checkoutUtility=new CheckoutUtility(session);
+                if(product != null){
+                    request.setAttribute("product", product);
 
-            if(cart != null) {
-                int sizeCart = checkoutUtility.sizeCart(cart);
-                request.getSession().setAttribute("sizeCart", sizeCart);
-            }else{
-                request.getSession().setAttribute("sizeCart", 0);
+                    List<Item> cart= (List<Item>) request.getSession(false).getAttribute("cartProduct");
+                    CheckoutUtility checkoutUtility=new CheckoutUtility(session);
+
+                    if(cart != null) {
+                        int sizeCart = checkoutUtility.sizeCart(cart);
+                        request.getSession().setAttribute("sizeCart", sizeCart);
+                    }else{
+                        request.getSession().setAttribute("sizeCart", 0);
+                    }
+
+                    ResultDao resultDao = new ResultDao();
+                    List<Category> mainCategories = resultDao.getMainCategories(session);
+                    request.setAttribute("mainCategories", mainCategories);
+
+                    tx.commit();
+                    RequestDispatcher rd = request.getRequestDispatcher("/view/customer/jsp/product.jsp");
+                    rd.forward(request, response);
+                }
+                else{
+                    // TODO: Error Page
+                    tx.commit();
+                }
+            }catch (Exception ex) {
+                ex.printStackTrace();
+                tx.rollback();
             }
-
-
         }
-
-        ResultDao resultDao = new ResultDao();
-        List<Category> mainCategories = resultDao.getMainCategories(session);
-        request.setAttribute("mainCategories", mainCategories);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/view/customer/jsp/product.jsp");
-        rd.forward(request, response);
 
         /*Nouran Habib*/
 
