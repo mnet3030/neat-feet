cb6a20e8fba243d33b0a188381a4c9f37ff2052a
Mahmoud Shereif <Eng.Mahmoud.Shereif@gmail.com>
2019-04-23 04:23:58 +0200

    add categories, user roles, and countries at startup

--- src/main/java/com/imagine/neatfeat/controller/customer/listeners/ContextListener.java
+++ src/main/java/com/imagine/neatfeat/controller/customer/listeners/ContextListener.java
@@ -1,5 +1,9 @@
 package com.imagine.neatfeat.controller.customer.listeners;
 
+import com.imagine.neatfeat.model.dal.dao.SingletonSessionFactory;
+import com.imagine.neatfeat.model.dal.entity.Category;
+import com.imagine.neatfeat.model.dal.entity.Country;
+import com.imagine.neatfeat.model.dal.entity.UserRoles;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -14,6 +18,30 @@ public class ContextListener implements ServletContextListener {
         /*Mahmoud Shereif*/
        /* SessionFactory sessionFactory = new Configuration().configure("cfg/hibernate.cfg.xml").buildSessionFactory();
         servletContextEvent.getServletContext().setAttribute("sessionFactory", sessionFactory);*/
+        Session session = SingletonSessionFactory.getInstance().openSession();
+        Category category = new Category();
+        category.setDescription("man");
+        Category category1 = new Category();
+        category1.setDescription("woman");
+        session.beginTransaction();
+        session.persist(category);
+        session.persist(category1);
+
+        UserRoles userRole = new UserRoles();
+        userRole.setDescription("customer");
+        UserRoles userRole1 = new UserRoles();
+        userRole1.setDescription("admin");
+        session.persist(userRole1);
+
+        Country country = new Country();
+        country.setName("Egypt");
+        Country country1 = new Country();
+        country1.setName("Saudi");
+        session.persist(country);
+        session.persist(country1);
+        session.getTransaction().commit();
+
+        session.close();
 
         /*Amr El Kady*/
 
